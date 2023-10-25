bee33232560951de8dfd6e62b58e2aa4bda3a72f
Min Marn Oo <minmarnoo@gmail.com>
2023-10-25 13:07:01 +0800

    installing antd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 
-function App() {
+const App = () => {
+
+  console.log(process.env.REACT_APP_API_KEY)
+
   return (
     <div className="App">
       <header className="App-header">
