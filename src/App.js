153ec43ee225040fbc89823a6110601a692e55eb
Min Marn Oo <minmarnoo@gmail.com>
2023-10-25 12:54:20 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Welcome to Health Declaration App
+      </header>
+    </div>
+  );
+}
+
+export default App;
